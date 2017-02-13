717492e926260d8801ea372a45156cbe2ff957f9
Martin Gill <martin@martinsgill.co.uk>
2017-02-13 14:30:24 +0100

    initial drop area for statements

--- src/frontend/component/UploadStatements.tsx
+++ src/frontend/component/UploadStatements.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import { Card, CardTitle, CardText, List, ListItem } from "react-toolbox";
+import * as DropZone from 'react-dropzone';
 
 // No props, so use undefined
 // State is never set so we use the 'undefined' type.
 export class UploadStatements extends React.Component<undefined, undefined> {
+    
+    private onDrop (files: string[]) {
+      console.log('Received files: ', files);        
+    }
     render() {
         return <Card>
             <CardTitle
@@ -14,8 +19,12 @@ export class UploadStatements extends React.Component<undefined, undefined> {
                     <ListItem
                         caption="Upload statements to this page"
                         leftIcon="file_upload"
-                    />
+                    >
+                    </ListItem>
                 </List>
+                <DropZone onDrop={this.onDrop}>
+                    <div>Try dropping some files here, or click to select files to upload.</div>
+                </DropZone>
             </CardText>
         </Card>;
     }
